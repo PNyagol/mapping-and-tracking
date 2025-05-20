abc82144a88345bb7ea7af917a8d50c1eabd5ad8
uhuru-rawlings <uhururawlings40@gmail.com>
2025-05-20 21:13:08 +0300

    Fix api base url

--- src/App.js
+++ src/App.js
@@ -38,8 +38,8 @@ const theme = createTheme({
 
 
 const httpLink = createHttpLink({
-  uri: "http://127.0.0.1:8000/graphql/",
-  // uri: "https://app.mazingiraconcept.com/graphql/",
+  // uri: "http://127.0.0.1:8000/graphql/",
+  uri: "https://app.mazingiraconcept.com/graphql/",
 });
 
 
